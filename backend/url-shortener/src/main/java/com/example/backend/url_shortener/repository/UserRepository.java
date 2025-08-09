53f527b172b3ee683d4b4a456c046524f26a2e7d
rakshita-battina <rakshita.battina@hotmail.com>
2025-08-09 12:03:16 +0530

    Delete backend directory

--- backend/url-shortener/src/main/java/com/example/backend/url_shortener/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.url_shortener.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.backend.url_shortener.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
